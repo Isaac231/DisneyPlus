8a56d6637b5de8951b6a0469b5a49b6938363421
Isaac231 <isaacoe89@gmail.com>
2023-07-05 20:46:25 -0600

    añadi detalles al link de registrarse y programe el script

---
+++ scripts/login.js
@@ -0,0 +1,10 @@
+
+function login(){
+    var user = document.getElementById("user").value;
+    var password = document.getElementById("password").value;
+    if (user == "user" && password == "user")
+    {
+       // location.replace("index.html");
+       location.href="index.html";
+    }
+}
