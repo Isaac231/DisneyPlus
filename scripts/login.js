441248a8be465308dfe0d8cfc04b9c739862c061
Isaac231 <isaacoe89@gmail.com>
2023-07-10 19:02:32 -0600

    ahora el usuario puede logear usando tecla enter

--- scripts/login.js
+++ scripts/login.js
@@ -8,3 +8,15 @@ function login(){
        location.href="index.html";
     }
 }
+var input = document.getElementById("password");
+input.addEventListener("keypress", function(login){
+    if(login.key ==="Enter"){
+        var user = document.getElementById("user").value;
+    var password = document.getElementById("password").value;
+    if (user == "user" && password == "user")
+    {
+       // location.replace("index.html");
+       location.href="index.html";
+    }
+    }
+});
